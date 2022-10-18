db5bdffa5b2d3d747952fc7e5baac616d3be298d
ernestinetshiamala <114005736+ernestinetshiamala@users.noreply.github.com>
2022-10-18 16:48:00 +0100

    fichier erreur 404

---
+++ 404.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>404</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="description" content="codedthemes">
+    <meta name="author" content="codedthemes">
+
+    <!-- Favicon icon -->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+    <!-- Google font-->
+   <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet">
+
+    <!--ico Fonts-->
+    <link rel="stylesheet" type="text/css" href="assets/icon/icofont/css/icofont.css">
+
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+    <!-- waves css -->
+    <link rel="stylesheet" type="text/css" href="assets/plugins/Waves/waves.min.css">
+
+         <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+    <!-- Responsive.css-->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+
+
+</head>
+<body>
+
+<div class="error-404">
+    <!-- Container-fluid starts -->
+    <div class="container-fluid">
+        <!-- Row start -->
+        <div class="row">
+            <div class="text-uppercase col-xs-12">
+                <h1>404</h1>
+                <h5>Page Not Found</h5>
+                <p>oops! page not found</p>
+                <a href="index.html" class="btn btn-error btn-lg waves-effect">back to home page</a>
+            </div>
+        </div>
+        <!-- Row end -->
+    </div>
+    <!-- Container-fluid ends -->
+</div>
+<!-- Required Jqurey -->
+<script src="assets/plugins/Jquery/dist/jquery.min.js"></script>
+
+<!-- tether.js -->
+<script src="assets/plugins/tether/dist/js/tether.min.js"></script>
+
+<!-- Required Framework -->
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- waves effects.js -->
+<script src="assets/plugins/Waves/waves.min.js"></script>
+
+
+
+</body>
+</html>
